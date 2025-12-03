e7071c9d788acd7727da99341e98e3d12bf7af80
kondziet <konrad.dabrowski54@gmail.com>
2025-12-03 01:04:52 +0100

    add solution for part 1 of day 1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -3,6 +3,26 @@ package pl.kondziet
 import java.io.File
 
 fun main() {
-    val lines = File("src/main/resources/day1.txt").readLines()
-    println(lines)
+    println(
+        """
+        day 1
+        part 1: ${part1()}
+        """
+    )
+}
+
+fun part1(): Int {
+    val lines = File("src/main/resources/day1-p1.txt").readLines()
+
+    return lines.asSequence()
+        .map { rotation ->
+            val direction = rotation[0]
+            val clicks = rotation.substring(1).toInt()
+            if (direction == 'L') -clicks else clicks
+        }
+        .runningFold(50) { currentPosition, turn ->
+            (currentPosition + turn).mod(100)
+        }
+        .drop(1)
+        .count { it == 0 }
 }
